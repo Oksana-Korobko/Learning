ff25cb84359f3f9ebf318294668f20358b0e778f
Oksana <oksana.korobko.fd@gmail.com>
2024-08-02 22:09:40 +0300

    clear

--- dist/scripts.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let digit = 5;
-let str = "2";
-let m = digit + str;
-let a = 45; // любой тип
-console.log(m);
-console.log(6);
-//# sourceMappingURL=scripts.js.map
--- scripts.ts
+++
@@ -1,7 +0,0 @@
-let digit: number = 5;
-let str: string = "2";
-let m = digit + str;
-let a: any = 45; // любой тип
-
-console.log(m)
-console.log(6)
