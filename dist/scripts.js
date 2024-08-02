798636556d806165e946e2f51ffc2ccf76cd7f66
Oksana <oksana.korobko.fd@gmail.com>
2024-08-02 15:43:27 +0300

    les1

---
+++ dist/scripts.js
@@ -0,0 +1,6 @@
+"use strict";
+let digit = 5;
+let str = "text";
+let m = digit + str;
+let a = 45;
+console.log(m);
---
+++ scripts.ts
@@ -0,0 +1,6 @@
+let digit: number = 5;
+let str: string = "text";
+let m = digit + str;
+let a: any = 45; // любой тип
+
+console.log(m)
