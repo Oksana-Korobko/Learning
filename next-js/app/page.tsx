2753b1115523d2122f4262050e18766fae989b1e
Oksana <oksana.korobko.fd@gmail.com>
2024-08-05 01:01:10 +0300

    mainPage1

--- next-js/app/page.tsx
+++ next-js/app/page.tsx
@@ -1,7 +1,17 @@
+import books from './books.json';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello! </h1>
-    </div>
+    <main>
+      <h1>Hello in Books World! </h1>
+      <ul>
+        {books.map((book) => {
+          return <li key={book.id}>
+            <h2>{book.title}</h2>
+            {book.author} 
+          </li>
+        })}
+      </ul>
+    </main>
   );
 }
